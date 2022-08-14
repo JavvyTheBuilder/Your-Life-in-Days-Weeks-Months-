471099d18de450d32d870bc62e2850223c0b7e5d
Jae Williams <97705866+JavvyTheBuilder@users.noreply.github.com>
2022-08-14 20:30:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? \n ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+age_as_int = int(age)
+
+years_remain = 90 - age_as_int
+days_remain = years_remain * 365
+weeks_remain = years_remain * 52
+months_remain = years_remain * 12
+
+
+print(f" you have {days_remain} days, {weeks_remain} weeks, {months_remain} months, left.")
+
+
+
+
+
+
+
